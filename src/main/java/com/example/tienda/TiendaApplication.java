0d141d485b610eddcfdcc19d994d424bde421bbe
frenkiieeee <frank.c0819@gmail.com>
2026-04-27 09:52:00 +0200

    update: Main

--- src/main/java/com/example/tienda/TiendaApplication.java
+++ src/main/java/com/example/tienda/TiendaApplication.java
@@ -1,4 +1,13 @@
 package com.example.tienda;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class TiendaApplication {
+    // SpringApplication.run() arranca el servidor Tomcat embebido,
+    // inicializa el contexto de Spring, conecta con la base de datos
+    // y deja la aplicación escuchando en http://localhost:8080
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
