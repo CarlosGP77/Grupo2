c799ce39a2a5f2382b30b9b72a1499cbf75dce14
XDX8 <xdx82007@gmail.com>
2026-04-27 09:47:42 +0200

    Crecion de las entidades Producto y Pedido como su relacion N:M

--- src/main/java/com/example/tienda/model/Producto.java
+++ src/main/java/com/example/tienda/model/Producto.java
@@ -1,4 +1,28 @@
 package com.example.tienda.model;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_producto;
+    private String nombre;
+    private double precio;
+
+    @ManyToMany(mappedBy = "productos")
+    private List<Pedido> pedidos = new ArrayList<>();
+
+    //getter y setter
+    public Long getId(){return id_producto;}
+    public void setId(Long id){this.id_producto=id;}
+    public String getNombre(){return nombre;}
+    public void setNombre(String nom){this.nombre=nom;}
+    public double getPrecio(){return precio;}
+    public void setPrecio(double precio){this.precio = precio;}
+    public List<Pedido> getPedidos(){return pedidos;}
+    public void setPedidos(List<Pedido> pedidos){this.pedidos = pedidos;}
+
 }
