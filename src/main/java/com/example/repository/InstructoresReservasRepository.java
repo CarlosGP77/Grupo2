8dd40defee31937ab73231d8779d8993eb239ba8
XDX8 <xdx82007@gmail.com>
2026-05-15 13:10:38 +0200

    Actualizar InstructoresReservasRepository: cambiar findByCurso por findByReserva

--- src/main/java/com/example/repository/InstructoresReservasRepository.java
+++ src/main/java/com/example/repository/InstructoresReservasRepository.java
@@ -2,7 +2,7 @@ package com.example.repository;
 
 import com.example.model.InstructoresReservas;
 import com.example.model.Instructor;
-import com.example.model.Curso;
+import com.example.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface InstructoresReservasRepository extends JpaRepository<InstructoresReservas, Integer> {
     List<InstructoresReservas> findByInstructor(Instructor instructor);
-    List<InstructoresReservas> findByCurso(Curso curso);
+    List<InstructoresReservas> findByReserva(Reserva reserva);
 }
 
