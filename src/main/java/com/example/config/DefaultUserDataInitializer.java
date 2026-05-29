f99f26b1ab3e30cfb15fcdcef35bd95b7ece5c5d
XDX8 <xdx82007@gmail.com>
2026-05-29 09:51:30 +0200

    Sync: cambios locales

--- src/main/java/com/example/config/DefaultUserDataInitializer.java
+++ src/main/java/com/example/config/DefaultUserDataInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class exDefaultUserDataInitializer implements CommandLineRunner {
+public class DefaultUserDataInitializer implements CommandLineRunner {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
